22f429a1a1923cbe623e0904c2d5df36ffb38a6a
Sacs001 <https://github.com/Sacs001/rsschool-cv.git>
2022-08-02 12:18:20 +0300

    popup add

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 let slide = document.querySelectorAll(".popular_destination_section .destination");
 let popularDestinationSlider = document.querySelector(".slider .popular_destination_slider")
+let slideSwitch = document.querySelectorAll(".slide_swicher");
 let count = 0;
 let width;
 
@@ -13,5 +14,22 @@ function init() {
     })
 
 }
+slideSwitch.forEach(item => {
+    item.addEventListener("click", (event) => {
+        console.log(event)
+        let classSlide = event.originalTarget.classList;
+        classSlide.forEach(item2 => {
+            classSlide.remove("active")
+            if (item2 !== 'active') {
+                classSlide.add("active")
+                console.log(classSlide)
+            }
+        });
+        console.log("tup")
+    })
+})
+function slideDot() {
 
+}
 window.addEventListener("resize", init)
+
