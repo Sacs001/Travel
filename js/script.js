a720afa7170dab2299ee064edd8a7fe6817fc511
Sacs001 <https://github.com/Sacs001/rsschool-cv.git>
2022-08-04 23:12:39 +0300

    slider #1

--- js/script.js
+++ js/script.js
@@ -1,71 +1,45 @@
-let slide = document.querySelectorAll(".popular_destination_section .destination");
-let popularDestinationSlider = document.querySelector(".slider .popular_destination_slider")
+
+// Slider 
+
+// for mobile
+
+let slide = document.querySelectorAll(".destination");
 let slideSwitch = document.querySelectorAll(".slide_swicher");
-let count = 0;
-let width;
+// let btnLeft = document.querySelector('.')
+// let btnRight = document.querySelector('.')
+let i = 1;
+let z = 1;
 
-function init() {
-    console.log(slide)
-    width = document.querySelector(".slider").offsetWidth;
-    popularDestinationSlider.getElementsByClassName.width = width * slide.length + "px";
-    slide.forEach(item => {
-        item.getElementsByClassName.width = width + "px";
-        // item.style.height = "auto";
-    })
+// for Desktop
 
+slideSwitch.forEach(e => {
+    e.addEventListener('click', () => {
+        slideSwitch.forEach((elem) => {
+            elem.classList.remove("act")
+        })
+        e.classList.add('act')
+        i = e.id;
+        slide.forEach((slider) => {
+            slider.classList.remove("slide_active");
+        })
+        slide[i - 1].classList.add('slide_active');
+    });
+});
+
+// For mobile
+
+
+// btnLeft.addEventListener('click', function () {
+//     changer(i - 1);
+// });
+
+// btnRight.addEventListener('click', function () {
+//     changer(i + 1);
+// });
+
+function changer() {
+    slideSwitch[i - 1].classList.remove('act');
+    slideSwitch[i].classList.add('act');
+    slide[i].classList.add('slide_active');
+    slide[i - 1].classList.remove('slide_active')
 }
-slideSwitch.forEach(item => {
-    item.addEventListener("click", (event) => {
-        console.log(event)
-        let classSlide = event.originalTarget.classList;
-        classSlide.forEach(item2 => {
-            classSlide.remove("active")
-            if (item2 !== 'active') {
-                classSlide.add("active")
-                console.log(classSlide)
-            }
-        });
-        console.log("tup")
-    })
-})
-function slideDot() {
-
-}
-window.addEventListener("resize", init)
-
-
-
-
-
-
-// other
-
-
-function converter(num, sysInit, sysConvert) {
-
-    if (sysInit == 0) { }
-
-    let number = num;
-    let result = 0;
-    number = String(num)
-
-    let index = number.length;
-    for (let i = 0; i < number.length; i++) {
-        index--;
-        result += +number[i] * Math.pow(2, index);
-    }
-    console.log(result)
-
-    // let binary = (num % 2).toString()
-    // for (; num > 1;) {
-    //     num = parseInt(num / 2)
-    //     binary = (num % 2) + binary
-    // }
-    // console.log(binary)
-
-}
-
-converter(1101);
-converter(1111);
-// converter(14);
-// converter(15);
