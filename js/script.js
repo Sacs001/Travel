36fcb2107d8a3cc62c88afb1d2d539a5c5bf7fe5
Sacs001 <https://github.com/Sacs001/rsschool-cv.git>
2022-08-09 15:39:48 +0300

    popup close/open

--- js/script.js
+++ js/script.js
@@ -5,41 +5,105 @@
 
 let slide = document.querySelectorAll(".destination");
 let slideSwitch = document.querySelectorAll(".slide_swicher");
-// let btnLeft = document.querySelector('.')
-// let btnRight = document.querySelector('.')
-let i = 1;
+let btnLeft = document.querySelector('.arrow_left_slider')
+let btnRight = document.querySelector('.arrow_right_slider')
+let i = 0;
 let z = 1;
 
 // for Desktop
 
+function removeActAndAction() {
+
+    slideSwitch.forEach((elem) => {
+        elem.classList.remove("act")
+    });
+    slide.forEach((slider) => {
+        slider.classList.remove("slide_active");
+    });
+};
+
 slideSwitch.forEach(e => {
     e.addEventListener('click', () => {
-        slideSwitch.forEach((elem) => {
-            elem.classList.remove("act")
-        })
+        removeActAndAction()
         e.classList.add('act')
         i = e.id;
-        slide.forEach((slider) => {
-            slider.classList.remove("slide_active");
-        })
-        slide[i - 1].classList.add('slide_active');
+        slide[i].classList.add('slide_active');
     });
 });
 
+
 // For mobile
 
+btnLeft.addEventListener('click', function () {
+    i--;
+    if (i == -1) i = 2;
+    changer();
 
-// btnLeft.addEventListener('click', function () {
-//     changer(i - 1);
-// });
+});
 
-// btnRight.addEventListener('click', function () {
-//     changer(i + 1);
-// });
+btnRight.addEventListener('click', function () {
+    i++;
+    if (i == 3) i = 0;
+    changer();
+});
 
 function changer() {
-    slideSwitch[i - 1].classList.remove('act');
+    removeActAndAction()
     slideSwitch[i].classList.add('act');
     slide[i].classList.add('slide_active');
-    slide[i - 1].classList.remove('slide_active')
 }
+
+
+
+// sing_up
+
+let popupLogin = document.querySelector('.popup_login')
+let popupSingUp = document.querySelector('.popup_sing_up')
+
+function styleChange(clas) {
+    setTimeout(() => {
+        clas.classList.toggle('popup_close')
+    }
+        , 300)
+};
+
+
+document.querySelector('.btn_login').addEventListener('click', (ev) => {
+    styleChange(popupLogin)
+
+});
+
+document.querySelector('.register').addEventListener('click', () => {
+    styleChange(popupSingUp)
+});
+
+document.querySelector(".forgot_your_pass").addEventListener('click', (event) => {
+    event.preventDefault()
+});
+document.querySelector(".register").addEventListener('click', (event) => {
+    event.preventDefault()
+});
+
+document.querySelector(".register_log_in").addEventListener('click', (event) => {
+    event.preventDefault()
+    styleChange(popupSingUp)
+});
+
+
+// Close
+
+document.addEventListener('click', (event) => {
+    if (event.target.classList.value == "popup_login") {
+        styleChange(popupLogin)
+    } else if (event.target.classList.value == "popup_sing_up") {
+        styleChange(popupSingUp)
+    }
+    console.log(event.target.classList.value)
+});
+
+document.addEventListener('keydown', (event) => {
+    if (event.keydown == 27) {
+        styleChange(popupLogin)
+        console.log(event.keydown)
+    }
+});
